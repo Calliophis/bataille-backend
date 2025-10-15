d803514c38f7682eb94b6f9fff02eaefded3af33
Lisa <lisa.auger2@gmail.com>
2025-10-15 11:35:28 +0200

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   
   app.enableCors({
-    origin: 'https://bataille.vercel.app/',
-    allowedHeaders: 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
+    origin: 'https://bataille.vercel.app',
     methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
   })
 
