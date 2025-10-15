6f810f6317b3d9fb6c38019341fab0aa9bacad3a
Lisa <lisa.auger2@gmail.com>
2025-10-15 14:04:35 +0200

    fix: try again env file

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule);
+  const frontendUrl = process.env.FRONTEND_URL;
   
   app.enableCors({
-    origin: 'https://bataille.vercel.app',
+    origin: frontendUrl,
     methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
   })
 
